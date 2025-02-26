f71ff6f8f6236a7d3734ec5fc53f6caf9376e0eb
Tanner B <sonictanpat@gmail.com>
2025-02-26 14:14:06 -0600

    adding postman

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+require('./db/mongoose.js')
+const User = require('./src/db/models/user.js')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() =>{
+        res.send(user)
+    }).catch((error) => {
+        console.log('Error saving user:', error)
+        res.status(400).send(error)
+
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,27 +4,8 @@ mongoose.connect( 'mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true
 })
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
+const mongoose = require('mongoose')
 
-// const me = new User({
-//     name: 'Tanner',
-//     age: 19
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
-
-const validator = require('validator')
 const Task = mongoose.model('Task', {
     description: {
         type: String
@@ -34,49 +15,12 @@ const Task = mongoose.model('Task', {
     }
     })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
+
 const job = new Task({
     description: "Do the work",
     completed: true
 })
-
+// video 11-6
 const user = new User({
     name: 'Mike',
     email: 'myemail@mead.io',
