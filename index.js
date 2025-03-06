48fd76d72c0523cb2a08df6fe12822ccb6d30d56
Tanner B <sonictanpat@gmail.com>
2025-03-06 17:27:23 -0600

    basic encrypting of passwords

--- index.js
+++ index.js
@@ -12,4 +12,22 @@ app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
-}); // continue on folder 12
+})
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
+//12-3
+// andrew -> poijdsfjmorewoijwer -> andrew
+// mypass -> mpoijsaerjweroiwem
